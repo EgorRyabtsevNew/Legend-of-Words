8757a470b65be695365e48c0466412af95295a8a
EgorRyabtsevNew <oraclekus11@gmail.com>
2023-07-29 02:13:09 +0200

    0.4 - a
    Major update. Testing the new map and movement.

--- public/map_script.js
+++ public/map_script.js
@@ -12,6 +12,75 @@ stage.add(layer);
 let players = {};
 
 const tileSize = 50;
+const mapWidthInTiles = 50;
+const mapHeightInTiles = 50;
+const borderWidth = 5;
+const mapWidth = mapWidthInTiles * tileSize;
+const mapHeight = mapHeightInTiles * tileSize;
+const mapBorderWidth = borderWidth / 2;
+const mapBorderColor = 'black';
+
+const totalMapWidth = mapWidth + window.innerWidth;
+const totalMapHeight = mapHeight + window.innerHeight;
+
+let isDragging = false;
+let lastPointerPosition;
+
+stage.on('mousedown', (e) => {
+  if (e.evt.button === 0) {
+    isDragging = true;
+    lastPointerPosition = stage.getPointerPosition();
+  }
+});
+
+stage.on('mouseup', () => {
+  isDragging = false;
+});
+
+stage.on('mousemove', (e) => {
+  if (isDragging) {
+    const dx = e.evt.clientX - lastPointerPosition.x;
+    const dy = e.evt.clientY - lastPointerPosition.y;
+    const newX = stage.x() + dx;
+    const newY = stage.y() + dy;
+
+    const minX = window.innerWidth - mapWidth;
+    const minY = window.innerHeight - mapHeight;
+    const maxX = 0;
+    const maxY = 0;
+
+    stage.position({
+      x: Math.min(maxX, Math.max(minX, newX)),
+      y: Math.min(maxY, Math.max(minY, newY)),
+    });
+
+    lastPointerPosition = stage.getPointerPosition();
+    layer.draw();
+  }
+});
+
+let selectedTile = null;
+let mousePressStartTime = 0;
+
+stage.on('mousedown', (e) => {
+  if (e.evt.button === 0) {
+    mousePressStartTime = Date.now();
+  }
+});
+
+stage.on('mouseup', (e) => {
+  if (Date.now() - mousePressStartTime > 500) {
+    const pos = stage.getPointerPosition();
+    const roundedX = Math.floor(pos.x / tileSize) * tileSize;
+    const roundedY = Math.floor(pos.y / tileSize) * tileSize;
+    selectedTile = { x: roundedX, y: roundedY };
+    const action = prompt('Select an action: Move');
+    if (action && action.toLowerCase() === 'move') {
+      socket.emit('updatePosition', { x: selectedTile.x, y: selectedTile.y });
+    }
+  }
+  mousePressStartTime = 0;
+});
 
 const urlParams = new URLSearchParams(window.location.search);
 const nickname = urlParams.get('nickname');
@@ -30,32 +99,45 @@ function drawTileBorders() {
     strokeWidth: 1,
   };
 
-  for (let x = tileSize; x < window.innerWidth; x += tileSize) {
+  for (let x = 0; x <= mapWidth; x += tileSize) {
     const line = new Konva.Line({
-      points: [x, 0, x, window.innerHeight],
+      points: [x, 0, x, mapHeight],
       ...lineAttrs,
     });
     layer.add(line);
   }
 
-  for (let y = tileSize; y < window.innerHeight; y += tileSize) {
+  for (let y = 0; y <= mapHeight; y += tileSize) {
     const line = new Konva.Line({
-      points: [0, y, window.innerWidth, y],
+      points: [0, y, mapWidth, y],
       ...lineAttrs,
     });
     layer.add(line);
   }
 }
 
+function drawMapBorder() {
+  const rect = new Konva.Rect({
+    x: mapBorderWidth,
+    y: mapBorderWidth,
+    width: mapWidth - borderWidth,
+    height: mapHeight - borderWidth + mapBorderWidth,
+    stroke: mapBorderColor,
+    strokeWidth: borderWidth,
+  });
+  layer.add(rect);
+}
+
 function updatePlayers() {
   layer.destroyChildren();
   drawTileBorders();
+  drawMapBorder();
 
   Object.keys(players).forEach((playerId) => {
     const player = players[playerId];
 
-    const x = Math.min((Math.floor(player.x / tileSize) + 0.5) * tileSize, window.innerWidth - 20);
-    const y = Math.min((Math.floor(player.y / tileSize) + 0.5) * tileSize, window.innerHeight - 20);
+    const x = Math.min((Math.floor(player.x / tileSize) + 0.5) * tileSize, mapWidth - 20);
+    const y = Math.min((Math.floor(player.y / tileSize) + 0.5) * tileSize, mapHeight - 20);
 
     const circle = new Konva.Circle({
       x,
